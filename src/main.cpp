87d6f244d6dde3aa7c52c4d6c09cdbcfc92873db
abraham <abraham.bherer-constant@polymtl.ca>
2020-10-08 20:05:45 -0400

    Added show_usage function

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <string>
+#include "mesh/generator/Parser.hpp"
+#include "mesh/generator/MeshReaderSU2.hpp"
+#include <filesystem>
+#include <fstream>
 
-int main(int argc, char* argv[])
-{
-    std::cout << "main is running" << std::endl;
+void show_usage() {
+	std::cerr << "Usage: "
+	          << "Options:\n"
+	          << "\t-h,--help\t\tShow this help message\n"
+	          << "\t-i,--input\tSpecify the file path for the SU2 path\n"
+
+	          << std::endl;
+}
+
+int main(int argc, char *argv[]) {
     
-    return 0;
+    std::string inpath;
+
+    for (int i = 1; i < argc; i++){
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help"))
+        {
+            show_usage();
+            return 0;
+        }
+        else if ((arg == "-i") || (arg == "--input"))
+        {
+            inpath = argv[++i];
+            std::cout << "\tinput mesh path = " << inpath << std::endl;
+        }
+    }
+
+	return 0;
 }
