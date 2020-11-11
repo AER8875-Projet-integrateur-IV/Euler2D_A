69f149dde324444f2353b0164e7f5f401d9e5508
abraham <abraham.bherer-constant@polymtl.ca>
2020-11-11 17:27:48 -0500

    added maxIter display

--- src/inputParser/InputParser.cpp
+++ src/inputParser/InputParser.cpp
@@ -153,6 +153,7 @@ void ees2d::io::InputParser::printAll() {
             << "m_SpecificHeat " <<m_SpecificHeat << "\n"
             << "m_scheme       "  <<m_scheme        << "\n"
             << "m_minResiudal  "  <<m_minResiudal   << "\n"
+            << "m_maxIter      "  <<m_maxIter   << "\n"
             << "m_threadsNum   "  <<m_threadsNum    << "\n"
             << "m_outputFormat " <<m_outputFormat << "\n"
             << "m_outputFile   "  <<m_outputFile    << "\n"
